2ee5bd499962db1a2aec24e019a378d5d172f79f
BlackKakapo <petrachialexandru@gmail.com>
2021-07-15 19:00:38 +0300

    Adding more pages, vavigation bar

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import io
-import spacy
 import csv
-from flask import Flask, render_template, request
+import spacy
 from spacy import displacy
+from flask import Flask, render_template, request
 from graphviz import Digraph
 from conllu import parse_incr
 from conllu import parse
@@ -13,72 +13,91 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-	conllu = ''
-	svg = ''
-
-	if request.method == 'POST':
-		if request.form['func'] == '1':		
-			conllu = request.form['conllu']
-			print(conllu)
-			svg = propDisplaySpacy(conllu)
-			return render_template('index.html', conllu = conllu, svg = svg)
-
-		elif request.form['func'] == '2':
-			conllu = request.form['conllu']
-			print(conllu)
-			svg = propDisplayGraph(conllu)
-			return render_template('index.html', conllu = conllu, svg = svg)
-
-
 	return render_template('index.html', conllu = conllu)
 
 
 
+@app.route('/GraphvizConvertor.html', methods = ['GET', 'POST'])
+def graphConvertor():
+	conllu = ''
+
+	if request.method == 'POST':
+		conllu = request.form['conllu']
+		svg = propDisplayGraph(conllu)
+		return render_template('GraphvizConvertor.html', conllu = conllu, svg = svg)
+
+	return render_template('GraphvizConvertor.html')
+
+
+@app.route('/SpacyConvertor.html', methods = ['GET', 'POST'])
+def spaCyConvertor():
+	if request.method == 'POST':		
+		prop = request.form['prop']
+		if prop == None:
+			return "Incorrect format"
+		else:
+			svg = propDisplaySpacy(prop)
+			return render_template('SpacyConvertor.html', prop = prop, svg = svg)
+
+	return render_template('SpacyConvertor.html')
+
 
 def propDisplayGraph(connlu):
 
-	sentences = parse(connlu)
+	try:
 
-	listOfID = []
-	listOfHead = []
-	listOfString = []
+		sentences = parse(connlu)
 
-	sentence = sentences[0]
-	for i in range(0,len(sentence)):
-		token = sentence[i]
-		listOfID.append(token['id'])
-		listOfHead.append(token['head'] - 1)
-		listOfString.append(token['form'])
+		listOfID = []
+		listOfHead = []
+		listOfString = []
+		listOfUPOS = []
 
-	dot = Digraph(comment='The Round Table', format = 'svg')
+		sentence = sentences[0]
+		for i in range(0,len(sentence)):
+			token = sentence[i]
+			listOfID.append(token['id'] - 1)
+			listOfHead.append(token['head'] - 1)
+			listOfString.append(token['form'])
+			listOfUPOS.append(token['upos'])
 
-	subgraph = Digraph(comment='The Round Table')
-	subgraph.attr(rank='same')
-	
-	for i in range(0,len(listOfHead) - 1):
-		subgraph.edge(str(listOfString[i]), str(listOfString[i + 1]))
+		dot = Digraph(comment='The Round Table', format = 'svg')
+		dot.graph_attr['rankdir'] = 'LR'
 
-	for i in range(0,len(listOfHead)):
-		if str(listOfString[listOfHead[i]]) == '0':
-			continue
-		else:
-			subgraph.edge(str(listOfString[i]), str(listOfString[listOfHead[i]]))
-			continue
+		subgraph = Digraph(comment='The Round Table')
+		# subgraph.attr(rank='same')
+		subgraph.edge_attr['style'] = 'invis'
+		
+		for i in range(0,len(listOfHead) - 1):
+			subgraph.edge(str(listOfString[i]), str(listOfString[i + 1]))
 
-	dot.subgraph(subgraph)
+		for i in range(0,len(listOfHead) - 1):
+			if str(listOfHead[i]) == '-1':
+				continue
+			elif str(listOfUPOS[i]) == 'PUNCT':
+				continue				
+			else:
+				dot.edge(str(listOfString[i]), str(listOfString[listOfHead[i]]))		
 
-	dot.format = 'svg'
-	dot.render()  
-	svg = dot.pipe().decode('utf-8').strip()
-	return svg
+		dot.subgraph(subgraph)
+
+		dot.format = 'svg'
+		dot.render()  
+		svg = dot.pipe().decode('utf-8').strip()
+		return svg
+	except:
+		return "Incorrect format"
 
 
-def propDisplaySpacy(conllu):
-	nlp = spacy.load("ro_core_news_lg")
-	doc1 = nlp(conllu)
-	options = {"compact": True, "bg": "white","color": "black", "font": "Source Sans Pro"}
-	svg = displacy.render(doc1, style="dep", page=False, options=options)
-	return svg
+def propDisplaySpacy(prop):
+	if prop != None:
+		nlp = spacy.load("ro_core_news_lg")
+		doc1 = nlp(prop)
+		options = {"compact": True, "bg": "white","color": "black", "font": "Source Sans Pro"}
+		svg = displacy.render(doc1, style="dep", page=False, options=options)
+		return svg
+	else:
+		return "Incorrect format"
 
 if __name__ == "__main__":
 	app.run(debug=True)
